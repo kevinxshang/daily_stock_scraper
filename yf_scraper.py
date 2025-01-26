f2f7c06c01a620c432b4f396cb6a99f2513af221
Kevin Shang <160940717+kevinxshang@users.noreply.github.com>
2025-01-25 23:03:39 -0500

    Workflow Test5

--- yf_scraper.py
+++ yf_scraper.py
@@ -27,8 +27,8 @@ def scrape_yahoo_finance_intraday():
                 interval='1m',
                 #start=start_date.strftime('%Y-%m-%d'),
                 #end=end_date.strftime('%Y-%m-%d')
-                start = '2025-01-21',
-                end = '2025-01-22'
+                start = '2025-01-22',
+                end = '2025-01-23'
             )
 
             if intraday_data.empty:
